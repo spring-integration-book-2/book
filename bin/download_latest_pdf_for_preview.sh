b4999e372790b4d49b099858de0952511ea72db1
Josh Long <josh@joshlong.com>
2021-05-25 21:28:34 -0700

    demo-tree

--- bin/download_latest_pdf_for_preview.sh
+++ bin/download_latest_pdf_for_preview.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash 
-curl -L https://github.com/spring-integration-book-2/book/raw/artifacts/ScreenPdfProducer/index-screen.pdf > preview.pdf
+PDF_FILE=$HOME/Desktop/preview.pdf
+curl -L https://github.com/spring-integration-book-2/book/raw/artifacts/ScreenPdfProducer/index-screen.pdf > $PDF_FILE
+open $PDF_FILE
